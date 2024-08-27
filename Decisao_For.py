b98dc9f722a05994f9146c2d62e97ceac3fcce36
unknown <vaniabeatriz999@gmail.com>
2024-08-27 14:04:30 -0300

    Exercicio laços de repetição

---
+++ Decisao_For.py
@@ -0,0 +1,5 @@
+tabuada=+int (input("Digite um numero para exibir a tabuada: "))
+print("Tabuada do número ", tabuada)
+
+for valor in range(1,11,1):
+    print(str(tabuada)+ " x " + str(valor) + " = " + str((tabuada*valor)))
---
+++ Decisao_while.py
@@ -0,0 +1,7 @@
+numero=int (input("Digite um número:"))
+
+while numero<100:
+    print("\t" + str(numero))
+    numero=numero+1
+
+print("Laço encerrado...")
